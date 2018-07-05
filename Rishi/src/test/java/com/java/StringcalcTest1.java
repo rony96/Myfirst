e1307f2ddf29650bee0a215564bb1046a2f49d1a
IBM <IBM@IBM-FULLSTACK20.mshome.net>
2018-07-05 17:20:54 +0530

    adding

---
+++ Rishi/src/test/java/com/java/StringcalcTest1.java
@@ -0,0 +1,14 @@
+package com.java;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringcalcTest1 {
+
+	@Test
+	public void testadd() {
+		Stringcalc.add("1,2");
+	}
+
+}
